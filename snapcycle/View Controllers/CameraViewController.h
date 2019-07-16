321d6a33a14fcd59b70f4432896a8631e4832f26
Emily Best <emilyabest@fb.com>
2019-07-16 13:56:04 -0700

    View Controller Wireframes Commit 2. Camera, Categories, Profile, and Details VC's added. All have segues except DetailsVC.

---
+++ snapcycle/View Controllers/CameraViewController.h
@@ -0,0 +1,17 @@
+//
+//  CameraViewController.h
+//  snapcycle
+//
+//  Created by emilyabest on 7/16/19.
+//  Copyright © 2019 Quirky Quokkas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CameraViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
