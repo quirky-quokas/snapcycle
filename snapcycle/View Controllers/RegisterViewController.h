224a5bce2b01f2b67d3a0a998f3a77a04b158478
Taylor Ka <taylorka@fb.com>
2019-07-17 15:56:29 -0700

    getPFFileFromImage in RegisterViewController.h

--- snapcycle/View Controllers/RegisterViewController.h
+++ snapcycle/View Controllers/RegisterViewController.h
@@ -7,10 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RegisterViewController : UIViewController
+
++ (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image;
 
 @end
 
