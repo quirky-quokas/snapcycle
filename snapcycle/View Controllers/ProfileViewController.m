c98c57bd37f2ef5b20897dae9942e8744e8056e7
Emily Best <emilyabest@fb.com>
2019-07-17 11:31:23 -0700

    ProfileVC: added profile image, welcome text, long scrollView

--- snapcycle/View Controllers/ProfileViewController.m
+++ snapcycle/View Controllers/ProfileViewController.m
@@ -7,16 +7,37 @@
 //
 
 #import "ProfileViewController.h"
+#import "Parse/Parse.h"
 
 @interface ProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UILabel *welcomeLabel;
 
 @end
 
 @implementation ProfileViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad { //TODO: break up code chunks into methods
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // set the scrollView frame
+    self.scrollView.contentSize = CGSizeMake(375, 1300);
+    
+    // set the profile picture
+    self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
+    if (PFUser.currentUser[@"profileImage"]) { //TODO: potentially update with getting profile image from SnapUser instead of Parse database...
+        PFFileObject *imageFile = PFUser.currentUser[@"profileImage"];
+        UIImage *image = [[UIImage alloc] initWithData:imageFile.getData];
+        self.profileImage.image = image;
+    }
+    
+    // set the welcome text
+    self.welcomeLabel.text = [NSString stringWithFormat:@"Welcome %@!", PFUser.currentUser.username];
+    
+    // display a graph of their three trash piles
+    
+        
 }
 
 /*
