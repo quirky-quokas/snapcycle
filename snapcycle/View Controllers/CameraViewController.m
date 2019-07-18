09aea39401f3bdc471b7a443d1e24c19a9af79d4
Emily Best <emilyabest@fb.com>
2019-07-18 16:08:57 -0700

    CameraVC: working on segue to detailsVC

--- snapcycle/View Controllers/CameraViewController.m
+++ snapcycle/View Controllers/CameraViewController.m
@@ -132,7 +132,10 @@
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     DetailsViewController *detailsViewController = [segue destinationViewController];
-    // send image property
+    
+    PFQuery *categoryQuery = [PFQuery queryWithClassName:@"Category"];
+    detailsViewController.category = [categoryQuery getObjectWithId:@"u42Xiik8ok"];
+    detailsViewController.image = self.capturedImage;
     detailsViewController.delegate = self;
 }
  
