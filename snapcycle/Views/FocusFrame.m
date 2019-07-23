cc4df55584a11f5d16bd699575471c225baef882
Emily Best <emilyabest@fb.com>
2019-07-23 16:07:05 -0700

    CameraVC: Focus frame appears over point where user tapped to focus.

---
+++ snapcycle/Views/FocusFrame.m
@@ -0,0 +1,43 @@
+//
+//  FocusFrame.m
+//  snapcycle
+//
+//  Created by emilyabest on 7/23/19.
+//  Copyright © 2019 Quirky Quokkas. All rights reserved.
+//
+
+#import "FocusFrame.h"
+
+@implementation FocusFrame
+
+/**
+ Overwriting UIView's initWithFrame to initalize an instance of a FocusFrame UIView with the desired characteristics.
+ */
+- (UIView *)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        // make the frame
+        [self setBackgroundColor:[UIColor clearColor]];
+        [self.layer setBorderWidth:2.0];
+        [self.layer setBorderColor:[UIColor whiteColor].CGColor];
+        
+        // add animation?
+        CABasicAnimation *selectionAnimation = [CABasicAnimation animationWithKeyPath:@"borderColor"];
+        selectionAnimation.toValue = (id)[UIColor blueColor].CGColor;
+        selectionAnimation.repeatCount = 8;
+        [self.layer addAnimation:selectionAnimation forKey:@"selectionAnimation"];
+    }
+    
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
