0cc5c2b6f5e58d77c1116cd0068eea72aa5f1d25
Emily Best <emilyabest@fb.com>
2019-07-25 11:13:49 -0700

    CameraView: about to try using delegates for the camera!

---
+++ snapcycle/Views/CameraView.h
@@ -0,0 +1,17 @@
+//
+//  CameraView.h
+//  snapcycle
+//
+//  Created by emilyabest on 7/25/19.
+//  Copyright © 2019 Quirky Quokkas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CameraView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ snapcycle/Views/CameraView.m
@@ -0,0 +1,21 @@
+//
+//  CameraView.m
+//  snapcycle
+//
+//  Created by emilyabest on 7/25/19.
+//  Copyright © 2019 Quirky Quokkas. All rights reserved.
+//
+
+#import "CameraView.h"
+
+@implementation CameraView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
