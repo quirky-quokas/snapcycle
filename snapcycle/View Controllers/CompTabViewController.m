699cf8438eb98ac11ef3de7517e604dfd16c1796
Taylor Ka <taylorka@fb.com>
2019-08-01 18:07:27 -0700

    re-refator snapcycle code

--- snapcycle/View Controllers/CompTabViewController.m
+++ snapcycle/View Controllers/CompTabViewController.m
@@ -27,8 +27,7 @@
     [super viewDidLoad];
     
     // set the navigation bar font
-    UIColor *scBlue = [UIColor colorWithRed:0.0/255.0 green:112.0/255.0 blue:194.0/255.0 alpha:1.0];
-    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:scBlue, NSFontAttributeName:[UIFont fontWithName:@"SourceSansPro-Light" size:25]}];
+    [TabBarController setSnapcycleLogoTitleForNavigationController:self.navigationController];
     
     [self setupTabsDesign];
     [self instantiateVCs];
